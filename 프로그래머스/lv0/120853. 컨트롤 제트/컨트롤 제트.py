edb096520da0eaaa7759bc5957783fc181c2a083
Kwonsu Kim <66168725+Kuneosu@users.noreply.github.com>
2023-06-13 00:18:48 +0900

    [level 0] Title: 컨트롤 제트, Time: 0.03 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120853. 컨트롤 제트/컨트롤 제트.py
@@ -0,0 +1,10 @@
+def solution(s):
+    answer = 0
+    nums = s.split()
+    front = 0
+    for i in range(len(nums)):
+        if nums[i] != "Z":
+            answer += int(nums[i])
+        else:
+            answer -= int(nums[i-1])
+    return answer
