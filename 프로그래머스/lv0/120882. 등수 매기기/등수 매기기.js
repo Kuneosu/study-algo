53b90307e0a99692f59b5a14cc2d8bc5955dec64
Kwonsu Kim <66168725+Kuneosu@users.noreply.github.com>
2023-06-28 14:13:19 +0900

    [level 0] Title: 등수 매기기, Time: 0.12 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120882. 등수 매기기/등수 매기기.js
+++ 프로그래머스/lv0/120882. 등수 매기기/등수 매기기.js
@@ -1,23 +1,20 @@
-function updateArray(myArray, oldValue, newValue) {
-    if (!myArray instanceof Array) return;
-    const index = myArray.indexOf(oldValue);
-    if (index !== -1) {
-        myArray[index] = newValue;
-    }
-}
+const score = [[80, 70], [70, 80], [30, 50], [90, 100], [100, 90], [100, 100], [10, 30]];
 
 function solution(score) {
-    const avg = [];
-    for (let item of score) {
-        avg.push((item[0] + item[1]) / 2)
-    }
+    // 평균 값을 저장한 avg 배열 생성
+    const avg = score.map(v => (v[0] + v[1]) / 2);
+    // 평균 값을 내림차순으로 정렬하여 저장해둘 sort_avg 배열 생성 
     let sort_avg = [...avg];
-    sort_avg.sort(function (a, b) {
-        return b - a;
-    });
+    sort_avg.sort((a, b) => b - a);
+
+    // 이전 값과 같은지 판단하는 변수
     let pre_item = 101;
+    // 등수 값을 가지고 있는 변수
     let rank = 1;
+    // 각 점수별 등수를 저장할 객체
     const ranks = {};
+
+    // 정렬된 배열을 순회하면서 이전값과 다른 값이라면 객체의 키값으로 추가하고 rank를 value로 저장
     for (let item of sort_avg) {
         if (item !== pre_item) {
             ranks[item] = rank;
@@ -27,8 +24,11 @@ function solution(score) {
             rank++;
         }
     }
-    
+
     const answer = [];
+
+    // 정렬되지 않은 평균값 배열을 순회하면서 
+    // 값에 맞는 value 값을 answer 배열에 저장.
     for (let it of avg) {
         it = it.toString();
         answer.push(ranks[it]);
@@ -36,3 +36,5 @@ function solution(score) {
 
     return answer;
 }
+
+console.log(solution(score));
