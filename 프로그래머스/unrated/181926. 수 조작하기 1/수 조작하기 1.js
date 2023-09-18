e0b35d58f4e804476850d9ab8473640e5129009b
Kwonsu Kim <66168725+Kuneosu@users.noreply.github.com>
2023-09-18 12:37:41 +0900

    [unrated] Title: 수 조작하기 1, Time: 3.80 ms, Memory: 37.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181926. 수 조작하기 1/수 조작하기 1.js
@@ -0,0 +1,22 @@
+function solution(n, control) {
+    for(con of control){
+        switch(con){
+            case 'w':
+                n += 1;
+                break;
+            case 'a':
+                n -= 10;
+                break;
+            case 's':
+                n -= 1;
+                break;
+            case 'd':
+                n += 10;
+                break;
+            default:
+                break;
+        }
+    }
+    
+    return n;
+}
