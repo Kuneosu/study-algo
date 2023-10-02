b3dcc87c71c619498b2979a3d1a2202dcd39bf60
Kwonsu Kim <66168725+Kuneosu@users.noreply.github.com>
2023-10-02 16:04:16 +0900

    [unrated] Title: 조건에 맞게 수열 변환하기 1, Time: 3.68 ms, Memory: 44.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181882. 조건에 맞게 수열 변환하기 1/조건에 맞게 수열 변환하기 1.js
@@ -0,0 +1,12 @@
+function solution(arr) {
+    let result = [];
+    for(let i=0;i<arr.length;i++){
+        let num = arr[i];
+        if(num>=50 && num%2===0){
+            arr[i] = num/2;
+        }else if(num<50 && num%2!=0){
+            arr[i] = num*2;
+        }
+    }
+    return arr;
+}
