46c6b0ed042e1871411be85547e957a101015e41
Kwonsu Kim <66168725+Kuneosu@users.noreply.github.com>
2023-10-09 22:10:32 +0900

    [level unrated] Title: 전국 대회 선발 고사, Time: 0.13 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181851. 전국 대회 선발 고사/README.md
+++ 프로그래머스/unrated/181851. 전국 대회 선발 고사/README.md
@@ -1,10 +1,10 @@
-# [unrated] 전국 대회 선발 고사 - 181851 
+# [level unrated] 전국 대회 선발 고사 - 181851 
 
 [문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181851) 
 
 ### 성능 요약
 
-메모리: 33.6 MB, 시간: 0.19 ms
+메모리: 33.4 MB, 시간: 0.13 ms
 
 ### 구분
 
@@ -12,7 +12,11 @@
 
 ### 채점결과
 
-Empty
+정확성: 100.0<br/>합계: 100.0 / 100.0
+
+### 제출 일자
+
+2023년 10월 1일 22:10:29
 
 ### 문제 설명
 
@@ -86,4 +90,4 @@ Empty
 </ul>
 
 
-> 출처: 프로그래머스 코딩 테스트 연습, https://programmers.co.kr/learn/challenges
+> 출처: 프로그래머스 코딩 테스트 연습, https://school.programmers.co.kr/learn/challenges
--- 프로그래머스/unrated/181851. 전국 대회 선발 고사/전국 대회 선발 고사.js
+++ 프로그래머스/unrated/181851. 전국 대회 선발 고사/전국 대회 선발 고사.js
@@ -1,21 +1,10 @@
 function solution(rank, attendance) {
-    let answer = 0;
-    const true_rank = [];
-    for (let i = 0; i < attendance.length; i++) {
-        if (attendance[i]) {
-            true_rank.push(rank[i]);
-        }
+    let abc = [];
+    for (let i = 1; i <= rank.length; i++) {
+        let index = rank.indexOf(i);
+        if (attendance[index]) {
+            abc.push(index);
+         }
     }
-
-    true_rank.sort(compareNumbers = (a, b) => {
-        return a - b;
-    });
-
-    const a = rank.findIndex(v => v === true_rank[0]);
-    const b = rank.findIndex(v => v === true_rank[1]);
-    const c = rank.findIndex(v => v === true_rank[2]);
-
-    answer = (10000 * a) + (100 * b) + c;
-
-    return answer;
+    return (10000*abc[0] + 100*abc[1] + abc[2]);
 }
